811da9338dedbf75b3ac5bcacafe2d41c2753bad
Victor <victor.alexanderzhao@gmail.com>
2022-06-24 13:59:37 -0300

    First commit

---
+++ testTarget5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace testTarget5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Victor";
+            char[] charArray = name.ToCharArray();
+            char pri = charArray[0];
+            char seg = charArray[1];
+            char ter = charArray[2];
+            char quar = charArray[3];
+            char quin = charArray[4];
+            char sext = charArray[5];
+            Console.Write(sext);
+            Console.Write(quin);
+            Console.Write(quar);
+            Console.Write(ter);
+            Console.Write(seg);
+            Console.Write(pri);
+        }
+    }
+}
